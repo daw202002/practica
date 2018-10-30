fc308463e4e2686c83b651e354cd4fadff850166
admin <admin@Win7_1TB>
2018-10-30 09:15:43 +0100

    Inicial

---
+++ Practica_Github/src/practica_github/Practica_Github.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica_github;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author admin
+ */
+public class Practica_Github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    String cadena;
+        String caracter;
+        int cont=0;
+        Scanner sc=new Scanner (System.in);
+        
+        System.out.println("Caracter: ");
+        caracter=sc.nextLine();
+        System.out.println("Escribe cadena: ");
+        cadena=sc.nextLine();
+        
+        for (int i=0; i<cadena.length();i++){
+            if (cadena.charAt(i)==caracter.charAt(0))
+                cont++;
+        }
+        
+        System.out.println(caracter+" aparece "+cont+" veces");
+    }
+}
